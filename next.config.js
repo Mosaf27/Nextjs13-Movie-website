c0efabcdc1d74a7aa680d8a9c27f0cf871ad299e
mosaf27 <shafiqsaf29@gmail.com>
2023-06-10 21:57:40 +0530

    adds mongodb connection to add users to database

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+    serverComponentsExternalPackages:["mongoose"],     //add mongoose as external server component package
+    
     images:{
         domains:['occ-0-3933-116.1.nflxso.net']         //images url for api
     }
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,25 @@
 
+import ContactCard from "../components/ContactCard"
+import styles from "./contact.module.css"
+import ContactForm from "../components/ContactForm"
+
 
 const Contact = ()=> {
     return(
-        <h1>Contact Page</h1>
+        <>
+        <div className={styles.container}>
+        <h1>Contact Us</h1>
+        <ContactCard />
+        </div>
+
+        <section className={styles.contact_section}>
+            <h2>We Love to hear <span>from you</span></h2>
+
+            <ContactForm />
+        </section>
+
+        
+        </>
     )
 }
 
